6ccb544b0be966a791166873fc8ecf474050cce9
Reuven Lerner <reuven@lerner.co.il>
2021-12-30 11:01:32 +0200

    gitautosync update 68

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+print(globals())
+
 print(f'[main] Starting main, {__name__=}')
 print('[main] About to import a')
 import a
 print('[main] Done importing a')
+
+print(globals())
+
 
 print('[main] About to import b')
 import b
