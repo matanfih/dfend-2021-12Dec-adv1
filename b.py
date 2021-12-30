15e483d9cc4f9d548eafff14f8ec5e57593e5ed4
Reuven Lerner <reuven@lerner.co.il>
2021-12-30 10:56:54 +0200

    gitautosync update 58

--- b.py
+++ b.py
@@ -3,9 +3,7 @@ print('[b] About to import a')
 import a
 print('[b] Done importing a')
 
-print('[a] Starting a, {__name__=}')
-
 x = 'hello from b'
 
 def hello(name):
-    return 'Hello from a, {name}!'
+    return 'Hello from b, {name}!'
