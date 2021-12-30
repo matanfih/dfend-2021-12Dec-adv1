48458d905ff79beb82e4f1b275370f4a9447697a
Reuven Lerner <reuven@lerner.co.il>
2021-12-30 10:20:28 +0200

    gitautosync update 45

--- mymod.py
+++ mymod.py
@@ -9,4 +9,9 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello, {name}!'
 
-print(f'Goodbye from {__name__}!')
+if __name__ == '__main__':
+    print(f'Goodbye from {__name__}!')
+
+    name = input('Enter your name: ')
+    print(hello(name))
+    
