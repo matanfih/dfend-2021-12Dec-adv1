5cb51f99cb6f7464b3c1d105684b5c4a02e7fb8c
Reuven Lerner <reuven@lerner.co.il>
2022-01-05 16:23:29 +0200

    gitautosync update 216

---
+++ myproc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from concurrent.futures import ThreadPoolExecutor, wait, as_completed
+import glob
+
+
+def file_size(filename):
+    total = 0
+
+    for one_line in open(filename):
+        total += len(one_line)
+
+    if total % 2:
+        raise ValueError('Odd number of characters')
+
+    return total
+
+
+with ThreadPoolExecutor(max_workers=5) as executor:
+    results = executor.map(file_size,
+                           glob.glob('*.txt'))
+
+    for one_result in results:
+        print(one_result)
+
+print('Done!')
