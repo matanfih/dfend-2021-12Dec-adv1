f13a34b7aa8078c7a587b9909b93af208072a90c
Reuven Lerner <reuven@lerner.co.il>
2022-01-05 16:07:34 +0200

    gitautosync update 187

--- mythread3.py
+++ mythread3.py
@@ -22,6 +22,11 @@ with ThreadPoolExecutor(max_workers=5) as executor:
     done, not_done = wait(all_futures)
 
     for one_future in done:
-        print(one_future.result())
+        e = one_future.exception()
+
+        if e:
+            print(f'You got an exception: {e}')
+        else:
+            print(one_future.result())
 
 print('Done!')
