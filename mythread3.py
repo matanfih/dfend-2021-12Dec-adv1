abca2be6934b92c068283b5ef210fd56d5d8ac0e
Reuven Lerner <reuven@lerner.co.il>
2022-01-05 16:00:32 +0200

    gitautosync update 169

---
+++ mythread3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import threading
+import time
+import random
+import queue
+
+l = threading.Lock()
+
+
+def hello(n):
+    time.sleep(random.randint(0, 3))
+    with l:
+        print(f'{n} Hello')
+        print(f'{n} Goodbye')
+
+
+# start threads
+all_threads = []
+for i in range(10):
+    t = threading.Thread(target=hello, args=(i,))
+    all_threads.append(t)
+    t.start()
+
+# wait for threads
+for one_thread in all_threads:
+    one_thread.join()   # wait for this thread to end
+
+
+print('Done!')
