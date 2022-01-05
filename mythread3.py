bc6fa04cce34bfc6d90b0b4418ef3159c9dfd56a
Reuven Lerner <reuven@lerner.co.il>
2022-01-05 16:04:35 +0200

    gitautosync update 179

--- mythread3.py
+++ mythread3.py
@@ -9,6 +9,7 @@ def hello(n):
     time.sleep(random.randint(0, 3))
     print(f'{n} Hello')
     print(f'{n} Goodbye')
+    return f'{n} Done'
 
 
 with ThreadPoolExecutor(max_workers=5) as executor:
